d204b3fd1c5902c535793380b18ba19dc4b296cf
Vladislav <nthedy1993@gmail.com>
2023-07-13 16:08:26 +0100

    change link from local to open server

--- cypress.config.js
+++ cypress.config.js
@@ -2,3 +2,26 @@ module.exports = {
   'projectId': '4b7344',
   e2e: {},
 }
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'vpad1v',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://example.cypress.io/',
+    
+  },
+  env: {
+    navbarText: 'cypress.io',
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+});
