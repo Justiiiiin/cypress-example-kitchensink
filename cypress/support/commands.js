e21def1cbe598e5c6b97580b004872d28eccafaa
Vladislav <nthedy1993@gmail.com>
2023-07-13 16:19:18 +0100

    Test again

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -37,16 +37,3 @@ Cypress.Commands.add('getLocalStorage', (key) => {
         return window.localStorage.getItem(key)
     })
 })
-
-Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
-    if (options && options.sensetive) {
-        options.log = false
-
-        Cypress.log({
-            $el: element,
-            name: 'type',
-            message: '*'.repeat(text.length)
-        })
-    }
-    return originalFn(element, text, options)
-})
--- cypress/support/component.js
+++ cypress/support/component.js
@@ -18,3 +18,10 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import { mount } from 'cypress/react18'
+
+Cypress.Commands.add('mount', mount)
+
+// Example use:
+// cy.mount(<MyComponent />)
